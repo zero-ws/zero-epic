7bf8dc80883578d685bdf29d1a62c750a2b2c424
silentbalanceyh <silentbalanceyh@126.com>
2023-06-16 17:51:45 +0800

    ServerType

--- zero-ams/src/main/environment/io/horizon/eon/em/web/ServerType.java
+++ zero-ams/src/main/environment/io/horizon/eon/em/web/ServerType.java
@@ -3,9 +3,8 @@ package io.horizon.eon.em.web;
 /**
  * 服务器类型专用枚举，在不同场景定义为主不一样，针对不同容器其服务器的种类也有所区别
  * <pre><code>
- *     - http：      标准HTTP服务器
- *     - http3:      HTTP3服务器，基于QUIC协议的新服务器模式，这种模式下开的sock就是QUIC的，现阶段用于
- *                   SMAVE的 Jetty Server 中
+ *     - http：      标准HTTP服务器（HTTP的具体版本隐藏在配置中，简单说只要是HTTP那么它对应的服务器的类型都是HTTP）
+ *                   至于协议版本是 1、2、3 这个直接在配置中去反应，而不反应到类型中
  *     - sock：      WebSocket服务器
  *     - rx：        RxJava模式下的HTTP服务器，这种模式主要牵涉Vertx中的编程风格
  *     - ipc：       RPC服务器，内部通讯专用
@@ -14,7 +13,6 @@ package io.horizon.eon.em.web;
  */
 public enum ServerType {
     HTTP("http"),
-    HTTP3("http3"),
     SOCK("sock"),
     RX("rx"),
     IPC("ipc"),

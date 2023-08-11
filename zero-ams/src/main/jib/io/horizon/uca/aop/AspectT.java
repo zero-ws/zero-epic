ae805a6bb7b70c69da5f4bcce24dbd0d1626e371
silentbalanceyh <silentbalanceyh@126.com>
2023-08-11 19:18:48 +0800

    501 Issue of appId

--- zero-ams/src/main/jib/io/horizon/uca/aop/AspectT.java
+++ zero-ams/src/main/jib/io/horizon/uca/aop/AspectT.java
@@ -1,5 +1,6 @@
 package io.horizon.uca.aop;
 
+import io.horizon.eon.VName;
 import io.horizon.eon.em.typed.ChangeFlag;
 import io.horizon.fn.HFn;
 import io.horizon.util.HUt;
@@ -7,7 +8,6 @@ import io.vertx.core.Future;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
-import javax.xml.crypto.dsig.keyinfo.KeyName;
 import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -86,23 +86,34 @@ class AspectT {
     private <P, T> Function<T, Future<T>> buildFn(final P plugin, final JsonObject config) {
         return input -> {
             if (plugin instanceof Before) {
-                // Run Before
+                /*
+                 * Run Before 前置模式下不需要做参数编译
+                 * 此处的 input 就直接是输入参数了，做强制转换即可
+                 */
                 final Before before = (Before) plugin;
                 if (input instanceof JsonObject) {
-                    return before.beforeAsync((JsonObject) input, config)
+                    final JsonObject inputJ = (JsonObject) input;
+                    return before.beforeAsync(inputJ, config)
                         .compose(json -> Future.succeededFuture((T) json));
                 } else {
-                    return before.beforeAsync((JsonArray) input, config)
+                    final JsonArray inputA = (JsonArray) input;
+                    return before.beforeAsync(inputA, config)
                         .compose(array -> Future.succeededFuture((T) array));
                 }
             } else {
-                // Run After
+                /*
+                 * Run After 后置模式下需要对参数做编译
+                 * 此处的 input 中会包含 __input 请求参数，所以此处需要构造新的
+                 * 参数来针对后置插件进行操作，防止数据丢失的情况发生
+                 */
                 final After after = (After) plugin;
                 if (input instanceof JsonObject) {
-                    return after.afterAsync( newInput(input), config)
+                    final JsonObject inputJ = (JsonObject) input;
+                    return after.afterAsync(this.buildParameters(inputJ), config)
                         .compose(json -> Future.succeededFuture((T) json));
                 } else {
-                    return after.afterAsync((JsonArray) input, config)
+                    final JsonArray inputA = (JsonArray) input;
+                    return after.afterAsync(this.buildParameters(inputA), config)
                         .compose(array -> Future.succeededFuture((T) array));
                 }
             }
@@ -127,8 +138,17 @@ class AspectT {
         }).filter(Objects::nonNull).forEach(executor::add);
         return executor;
     }
-    private JsonObject newInput(Object input) {
-        JsonObject copyInput = ((JsonObject) input).copy();
-        return copyInput.mergeIn(((JsonObject) input).getJsonObject("__input"));
+
+
+    private JsonObject buildParameters(final JsonObject input) {
+        final JsonObject inputJ = HUt.valueJObject(input, true);
+        final JsonObject requestJ = HUt.valueJObject(input, VName.__.INPUT, true);
+        return inputJ.mergeIn(requestJ, true);
+    }
+
+    private JsonArray buildParameters(final JsonArray input) {
+        final JsonArray paramA = new JsonArray();
+        HUt.itJArray(input).forEach(inputJ -> paramA.add(this.buildParameters(inputJ)));
+        return paramA;
     }
 }
